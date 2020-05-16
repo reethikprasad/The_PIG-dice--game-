05765e6bc8946be02e164f2d8d6edfcd730ec8ac
Reethik Prasad <reethikprasad@gmail.com>
2020-05-16 16:50:17 +0530

    added final screen

--- app.js
+++ app.js
@@ -8,24 +8,16 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score, activePlayer, roundScore;
+var scores, activePlayer, roundScore;
 
-score=[0,0];
-roundScore=0;
-activePlayer=0;
 
+init();
 
 
 // document.querySelector('#current-'+ activePlayer).textContent = dice;
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
 
-document.querySelector('.dice').style.display='none';
-
-document.getElementById('score-0').textContent=0;
-document.getElementById('score-1').textContent=0;
-document.getElementById('current-0').textContent=0;
-document.getElementById('current-0').textContent=0;
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
@@ -44,16 +36,64 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 
     }
     else{
-        //next player
-        activePlayer ===0? activePlayer =1: activePlayer =0;
-        roundScore =0;
+        nextPlayer();
+         }
+});
 
-        document.getElementById('current-0').textContent='0';
-        document.getElementById('current-1').textContent='0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    //add to current player
+    scores[activePlayer] += roundScore;
+    //update ui
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //check winner
+    if (scores[activePlayer]>=20){
+        document.querySelector('#name-' + activePlayer).textContent = 'winner';
         document.querySelector('.dice').style.display='none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    }else{
+        //next
+    nextPlayer();  
     }
-})
+
+    
+});
+
+function nextPlayer(){
+    //next player
+    activePlayer ===0? activePlayer =1: activePlayer =0;
+    roundScore =0;
+
+    document.getElementById('current-0').textContent='0';
+    document.getElementById('current-1').textContent='0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display='none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+    scores=[0,0];
+roundScore=0;
+activePlayer=0;
+
+document.querySelector('.dice').style.display='none';
+
+document.getElementById('score-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.getElementById('current-0').textContent=0;
+document.getElementById('current-0').textContent=0;
+document.querySelector('#name-0').textContent = 'Player 1';
+document.querySelector('#name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('activce');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+}
